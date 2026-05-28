8c78113887f650e78fb53f01d2d68862e31410d3
FAZERSSF2 <dd97312a@gmail.com>
2026-05-27 21:16:40 -0300

    Delete assets directory

--- assets/js/security.js
+++
@@ -1,7 +0,0 @@
-if (window.trustedTypes && window.trustedTypes.createPolicy) {
-    window.trustedTypes.createPolicy('default', {
-        createHTML: (string) => string,
-        createScript: (string) => string,
-        createScriptURL: (string) => string
-    });
-}
